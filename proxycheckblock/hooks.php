f0e3a3fd6b7c51447b354ca1c1baf9bcf43b5a28
Max Base <MaxBaseCode@Gmail.Com>
2020-11-15 13:24:55 +0330

    Update hooks.php

--- proxycheckblock/hooks.php
+++ proxycheckblock/hooks.php
@@ -1,1 +1,20 @@
+<?php
+// Max Base
+// https://github.com/BaseMax/proxycheck-whmcs-plugin
 
+if (!defined('WHMCS')) {
+	die('This file cannot be accessed directly');
+}
+
+function siavashblock_config() {
+    $configarray = array(
+		'name' => 'WHMCS ProxyCheckBlock Integration',
+		'version' => '1.0',
+		'author' => 'MaxBase',
+		'description' => 'Integrate ProxyCheckBlock into WHMCS',
+		'language' => 'english'
+	);
+    return $configarray;
+}
+
+?>
